ce0f14580e2a586322923325e6b647aea9e49d95
shubhi3199 <shbhm3199@gmail.com>
2019-10-07 21:02:15 +0530

    setting up axios package & making asynchronous call to food2fork api using cors proxy.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,18 @@
-// Global app controller
-import num from './test';
-const x=23;
-console.log(`I imported ${num} from another module. variable x is ${x}`);
+import axios from 'axios';
+async function getResults(query) {
+    const proxy='https://cors-anywhere.herokuapp.com/';
+    const key='86c62ba554c83f7c37b99a90578e6a05';
+    try{
+        const result=await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes=result.data.recipes;
+        console.log(result);
+        console.log(recipes);
+    }catch (error) {
+        alert(error);
+    }
+
+}
+getResults('pizza');
+
+//
+//https://www.food2fork.com/api/search
--- src/js/test.js
+++
@@ -1,2 +0,0 @@
-console.log("IMPORTED MODULES");
-export default 69;
